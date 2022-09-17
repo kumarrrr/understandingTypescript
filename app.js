77d7cbd9b4e111b2e685d668ca736b330ee6b77a
NirmalKumar <er.kumarnirmal@gmail.com>
2022-09-18 02:02:04 +0530

    second ts code

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Here this begins....');
+function add(num1, num2) {
+    return num1 + num2;
+}
+var number1 = 5;
+var number2 = 10;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
 console.log('Here this begins....')
+
+function add(num1:number,num2:number){
+    return num1+num2;
+}
+
+const number1 = 5;
+const number2 = 10;
+
+const result = add(number1,number2)
+console.log(result);
